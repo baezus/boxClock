97f63e4e4f30a508d069ee120a29a8783757ba72
Elias <elias@baez.us>
2021-04-18 13:23:33 -0400

    Display looks MVP.

--- components/Countdown.js
+++ components/Countdown.js
@@ -2,23 +2,31 @@ import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 import { Animated, Text, StyleSheet, PixelRatio, Touchable } from 'react-native';
 import styles from '../styles/styles';
 import React from 'react';
+import 'react-native-svg';
 
 //this is 'react-native-countdown-circle-timer': https://www.npmjs.com/package/react-native-countdown-circle-timer
+
+let FONT_BACK_LABEL = 58;
+if (PixelRatio.get()<=2){
+  FONT_BACK_LABEL = 53;
+}
 
 const children = ({ remainingTime }) => {
   const minutes = Math.floor(remainingTime / 60);
   let seconds = remainingTime % 60;
   if (seconds < 10) {
-    seconds = '0' + seconds;
+    FONT_BACK_LABEL = 70
+    return `${seconds}`
+  } else if (PixelRatio.get()<=2){
+    FONT_BACK_LABEL = 53;
+  } else {
+    FONT_BACK_LABEL = 58;
   }
 
   return `${minutes}:${seconds}`;
 }
 
-let FONT_BACK_LABEL = 55;
-if (PixelRatio.get()<=2){
-  FONT_BACK_LABEL = 50;
-}
+
 
 const Countdown = ({ isPlaying }) => (
   <CountdownCircleTimer
@@ -28,7 +36,7 @@ const Countdown = ({ isPlaying }) => (
     onComplete={() => {
       return [true, 1000]
     }}
-    strokeWidth={18}
+    strokeWidth={13}
     duration={180}
     size={150}
     trailColor={'transparent'}
@@ -39,7 +47,7 @@ const Countdown = ({ isPlaying }) => (
     ]}
     >
       {({ remainingTime, animatedColor }) => (
-        <Animated.Text style={{ color: animatedColor, fontSize: FONT_BACK_LABEL, fontWeight: 700, fontFamily: 'Changa One', paddingRight: 3, paddingLeft: 3, }} className="animated_text">
+        <Animated.Text style={{ color: animatedColor, fontSize: FONT_BACK_LABEL, fontWeight: 700, fontFamily: 'Changa One', paddingRight: 4, paddingLeft: 4, }} className="animated_text">
           {children({remainingTime})}
         </Animated.Text>
       )}
